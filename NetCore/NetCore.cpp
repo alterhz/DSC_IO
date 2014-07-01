e03e3a14f0ac9ce2b000be492132ce435c17bdee
alterhz <550360139@qq.com>
2014-07-01 16:51:35 +0800

    build lib

--- NetCore/NetCore.cpp
+++ NetCore/NetCore.cpp
@@ -20,30 +20,30 @@ void INetEngine::Release()
 	pNetServer = NULL;
 }
 
-int main(int argc, char* argv[])
-{
-	CServerTest *pServerTest = new CServerTest;
-
-	INetEngine *pNetEngine = INetEngine::CreateNetEngine();
-	if (!pNetEngine)
-	{
-		return -1;
-	}
-
-	pServerTest->SetNetEngine(pNetEngine);
-
-	pNetEngine->RegDispatcher(pServerTest);
-	pNetEngine->SetPort(9000);
-	pNetEngine->Start();
-
-	while(1)
-	{
-		system_time &tNow = boost::get_system_time();
-		boost::thread::sleep( tNow + boost::posix_time::microsec(100)); 
-
-		pNetEngine->DoTick(1);
-	}
-
-	return 0;
-}
+//int main(int argc, char* argv[])
+//{
+//	CServerTest *pServerTest = new CServerTest;
+//
+//	INetEngine *pNetEngine = INetEngine::CreateNetEngine();
+//	if (!pNetEngine)
+//	{
+//		return -1;
+//	}
+//
+//	pServerTest->SetNetEngine(pNetEngine);
+//
+//	pNetEngine->RegDispatcher(pServerTest);
+//	pNetEngine->SetPort(9000);
+//	pNetEngine->Start();
+//
+//	while(1)
+//	{
+//		system_time &tNow = boost::get_system_time();
+//		boost::thread::sleep( tNow + boost::posix_time::microsec(100)); 
+//
+//		pNetEngine->DoTick(1);
+//	}
+//
+//	return 0;
+//}
 
